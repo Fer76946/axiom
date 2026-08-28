13d7645e1bf2b28003450237e89a9ec9c1250f58
Fer <fernie8915@gmail.com>
2026-08-28 16:27:56 +0800

    Add teacher page and protected routes

---
+++ src/pages/Teacher/Teacher.tsx
@@ -0,0 +1,12 @@
+import "./Teacher.css";
+
+function Teacher() {
+  return (
+    <section className="teacher-page">
+      <h1>Teacher Dashboard</h1>
+      <p>Student management will go here.</p>
+    </section>
+  );
+}
+
+export default Teacher;
